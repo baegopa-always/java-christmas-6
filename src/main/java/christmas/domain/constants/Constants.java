f79cf9c926723dbc0265d672c0ad429eac0ca1c9
dj <fjzl12@naver.com>
2023-11-13 02:25:08 +0900

    feat(Constants): 상수 추가 구현

---
+++ src/main/java/christmas/domain/constants/Constants.java
@@ -0,0 +1,10 @@
+package christmas.domain.constants;
+
+public class Constants {
+    public static final String DRINK = "Drink";
+    public static final String APPETIZER = "Appetizer";
+    public static final String MAIN_DISH = "MainDish";
+    public static final String DESSERT = "Dessert";
+    public static final int MIN_EA = 1;
+    public static final int MAX_EA = 20;
+}
