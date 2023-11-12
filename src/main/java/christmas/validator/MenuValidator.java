b265f7a17b5f759ca50e46f84b244d9599afcdc1
dj <fjzl12@naver.com>
2023-11-12 20:54:50 +0900

    feat(MenuValidator): 사용자 입력 메뉴 쉼표 단위로 구분되는지 검증하는 기능 구현

--- src/main/java/christmas/validator/MenuValidator.java
+++ src/main/java/christmas/validator/MenuValidator.java
@@ -1,15 +1,39 @@
 package christmas.validator;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MenuValidator {
-    public static int validate(String input) {
-        // 쉼표 단위로 구분,, 패턴 ?
+    private static Map<String, Integer> menuInventory;
+
+    public static Map<String, Integer> validate(String input) {
+        menuInventory = new HashMap();
+        List<String> splitedMenu = splitByComma(input); // 쉼표 단위로 구분,, 패턴 ?
         // 하이픈 단위로 구분
+        // 중복
         // 있는 메뉴인가
         // 메뉴의 개수가 1이상 20 이하인가
         // 전체 메뉴 개수가 20이하 인가
         // 해시맵으로 리턴
         // menu들은 enum으로 ? 매칭되는가
         // 음료만 주문했는가
-        return 0;
+        return menuInventory;
+    }
+
+    private static List<String> splitByComma(String input) {
+        try {
+            List<String> splitedMenu = List.of(input.split(","));
+            return splitedMenu;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new IllegalArgumentException("컴마로 안나뉨");
+        }
+    }
+
+    private static void splitByHypen(List<String> splitedMenu) {
+        for (String menu : splitedMenu) {
+            menu.split("-");
+        }
+
     }
 }
