855ed4924fb974c5dc2437209eeabcac5365db4a
dj <fjzl12@naver.com>
2023-11-12 20:04:56 +0900

    feat(MenuValidator): 메뉴 입력 시 검증 기능 스켈레톤 코드 작성

---
+++ src/main/java/christmas/validator/MenuValidator.java
@@ -0,0 +1,15 @@
+package christmas.validator;
+
+public class MenuValidator {
+    public static int validate(String input) {
+        // 쉼표 단위로 구분,, 패턴 ?
+        // 하이픈 단위로 구분
+        // 있는 메뉴인가
+        // 메뉴의 개수가 1이상 20 이하인가
+        // 전체 메뉴 개수가 20이하 인가
+        // 해시맵으로 리턴
+        // menu들은 enum으로 ? 매칭되는가
+        // 음료만 주문했는가
+        return 0;
+    }
+}
