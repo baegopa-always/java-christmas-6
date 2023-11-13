fc8306c174992a0bcbb20cb2c2211bf6601c25cd
dj <fjzl12@naver.com>
2023-11-13 16:36:36 +0900

    feat(Constants): 필요 상수 추가 구현

--- src/main/java/christmas/domain/constants/Constants.java
+++ src/main/java/christmas/domain/constants/Constants.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Constants {
     public static final int MIN_EA = 1;
     public static final int MAX_EA = 20;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
     public static final int MIN_DAY = 1;
     public static final int MAX_DAY = 31;
     public static final List<Integer> SPECIAL_DAY = List.of(3, 10, 17, 24, 25, 31);
@@ -12,5 +14,11 @@ public class Constants {
     public static final int DECEMBER = 12;
     public static final int MIN_PRICE_FOR_GIFT = 120_000;
     public static final int CHRISTMAS_DAY = 25;
+    public static final int SPECIAL_DISCOUNT_PRICE = 1000;
+    public static final String CHRISTMAS_DISCOUNT = "크리스마스 디데이 할인";
+    public static final String WEEKDAY_DISCOUNT = "평일 할인";
+    public static final String WEEKEND_DISCOUNT = "주말 할인";
+    public static final String SPECIAL_DISCOUNT = "특별 할인";
+    public static final String GIFT_EVENT = "증정 이벤트";
 
 }
