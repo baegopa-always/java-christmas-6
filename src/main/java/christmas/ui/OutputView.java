9c30a7170c84a56a0c5d6eb969a431795a55f60c
dj <fjzl12@naver.com>
2023-11-13 16:39:26 +0900

    refactor(OutputView): 사용자 뷰에 사용할 메시지 상수 사용

--- src/main/java/christmas/ui/OutputView.java
+++ src/main/java/christmas/ui/OutputView.java
@@ -1,31 +1,49 @@
 package christmas.ui;
 
+import static christmas.domain.constants.Message.EVENT_DATE;
+import static christmas.domain.constants.Message.INTRO;
+import static christmas.domain.constants.Message.ORDER_MENU;
+import static christmas.domain.constants.Message.MENU_INFO;
+import static christmas.domain.constants.Message.TOTAL_RAW_PRICE;
+import static christmas.domain.constants.Message.NONE;
+import static christmas.domain.constants.Message.GIFT_MENU;
+import static christmas.domain.constants.Message.BENEFIT_DETAILS;
+import static christmas.domain.constants.Message.BENEFIT;
+import static christmas.domain.constants.Message.TOTAL_BENEFIT_PRICE;
+import static christmas.domain.constants.Message.PRICE;
+import static christmas.domain.constants.Message.TOTAL_EXPECTED_PRICE;
+import static christmas.domain.constants.Message.DECEMBER_BADGE;
+
+import static christmas.domain.constants.Constants.ONE;
+
+import christmas.domain.PriceFormatter;
+import christmas.domain.constants.EventBadge;
 import christmas.domain.constants.Menu;
+
 
 import java.util.Map;
 
+
 public class OutputView {
     public void printIntro() {
-        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
-        // ...
+        System.out.println(INTRO);
     }
 
     public void printEventDate(int day) {
-        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", day);
-        // ...
+        System.out.printf(EVENT_DATE, day);
     }
 
     public void printMenu(Map<Menu, Integer> menuInventory) {
-        System.out.println("<주문 메뉴>");
+        System.out.println(ORDER_MENU);
         for (Map.Entry<Menu, Integer> menu : menuInventory.entrySet()) {
-            System.out.printf("%s %d개\n", menu.getKey().getName(), menu.getValue());
+            System.out.printf(MENU_INFO, menu.getKey().getName(), menu.getValue());
         }
         System.out.println();
     }
 
-    public void printTotalRawPrice() {
-        System.out.println("<할인 전 총주문 금액>");
-        // ...
+    public void printTotalRawPrice(int price) {
+        System.out.printf(TOTAL_RAW_PRICE, PriceFormatter.format(price));
+        System.out.println();
     }
 
     public void printGiftMenu() {
