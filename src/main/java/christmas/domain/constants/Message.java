c30b3f72037552e4811144d66b8c2d0e41350241
dj <fjzl12@naver.com>
2023-11-13 16:38:08 +0900

    refactor(Message): 사용자 뷰에 사용할 메시지 상수 구현

--- src/main/java/christmas/domain/constants/Message.java
+++ src/main/java/christmas/domain/constants/Message.java
@@ -1,6 +1,21 @@
 package christmas.domain.constants;
 
 public class Message {
-    public static final String INTRO = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    public static final String MENU_GUIDE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String PRICE = "%s원\n";
+    public static final String READ_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String READ_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String INTRO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String EVENT_DATE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n";
+    public static final String ORDER_MENU = "<주문 메뉴>";
+    public static final String MENU_INFO = "%s %d개\n";
+    public static final String TOTAL_RAW_PRICE = "<할인 전 총주문 금액>\n" + PRICE;
+    public static final String NONE = "없음\n";
+    public static final String GIFT_MENU = "<증정 메뉴>";
+    public static final String BENEFIT_DETAILS = "<혜택 내역>";
+    public static final String BENEFIT = "%s: " + PRICE;
+    public static final String TOTAL_BENEFIT_PRICE = "<총혜택 금액>";
+    public static final String TOTAL_EXPECTED_PRICE = "<할인 후 예상 결제 금액>";
+    public static final String DECEMBER_BADGE = "<12월 이벤트 배지>";
+
+
 }
