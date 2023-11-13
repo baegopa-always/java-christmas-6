6dd8cacfcd09be5c86ea528115319e2771b68150
dj <fjzl12@naver.com>
2023-11-13 14:25:58 +0900

    feat(EventDetails): 할인 전 총주문 금액 계산 기능 구현

--- src/main/java/christmas/domain/EventDetails.java
+++ src/main/java/christmas/domain/EventDetails.java
@@ -1,11 +1,16 @@
 package christmas.domain;
 
+import christmas.domain.constants.EventBadge;
 import christmas.domain.constants.Menu;
 
+import static christmas.domain.constants.Constants.SPECIAL_DAY;
 import static christmas.domain.constants.Constants.THIS_YEAR;
 import static christmas.domain.constants.Constants.DECEMBER;
+import static christmas.domain.constants.Constants.MIN_PRICE_FOR_GIFT;
+import static christmas.domain.constants.Constants.CHRISTMAS_DAY;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
 public class EventDetails {
@@ -20,6 +25,14 @@ public class EventDetails {
         this.day = day;
         this.date = LocalDate.of(THIS_YEAR, DECEMBER, day);
         this.dayOfWeek = date.getDayOfWeek().getValue() / 7;
+    }
+
+    public int calculateTotalPrice() {
+        int price = 0;
+        for (Map.Entry<Menu, Integer> menu : menuInventory.entrySet()) {
+            price += (menu.getKey().getPrice() * menu.getValue());
+        }
+        return price;
     }
 
 
