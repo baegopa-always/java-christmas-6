c79a226ab958a62a1c47a92e40d02f47a533d7fa
dj <fjzl12@naver.com>
2023-11-13 14:21:39 +0900

    refactor(Menu): 메뉴 카테고리 enum 타입으로 변경

--- src/main/java/christmas/domain/constants/Menu.java
+++ src/main/java/christmas/domain/constants/Menu.java
@@ -1,24 +1,30 @@
 package christmas.domain.constants;
 
+import static christmas.domain.constants.MenuCategory.APPETIZER;
+import static christmas.domain.constants.MenuCategory.MAINDISH;
+import static christmas.domain.constants.MenuCategory.DESSERT;
+import static christmas.domain.constants.MenuCategory.DRINK;
+
 public enum Menu {
-    SOUP("Appetizer", "양송이수프", 6_000),
-    TAPAS("Appetizer", "타파스", 5_500),
-    SALAD("Appetizer", "시저샐러드", 8_000),
-    STAKE("MainDish", "티본스테이크", 55_000),
-    BBQ("MainDish", "바비큐립", 54_000),
-    SEAFOOD_PASTA("MainDish", "해산물파스타", 35_000),
-    CHRISTMAS_PASTA("MainDish", "크리스마스파스타", 25_000),
-    CHOCO_CAKE("Dessert", "초코케이크", 15_000),
-    ICE_CREAM("Dessert", "아이스크림", 5_000),
-    ZERO_COKE("Drink", "제로콜라", 3_000),
-    RED_WINE("Drink", "레드와인", 60_000),
-    CHAMPAGNE("Drink", "샴페인", 25_000);
-    private final String category;
+    SOUP(APPETIZER, "양송이수프", 6_000),
+    TAPAS(APPETIZER, "타파스", 5_500),
+    SALAD(APPETIZER, "시저샐러드", 8_000),
+    STAKE(MAINDISH, "티본스테이크", 55_000),
+    BBQ(MAINDISH, "바비큐립", 54_000),
+    SEAFOOD_PASTA(MAINDISH, "해산물파스타", 35_000),
+    CHRISTMAS_PASTA(MAINDISH, "크리스마스파스타", 25_000),
+    CHOCO_CAKE(DESSERT, "초코케이크", 15_000),
+    ICE_CREAM(DESSERT, "아이스크림", 5_000),
+    ZERO_COKE(DRINK, "제로콜라", 3_000),
+    RED_WINE(DRINK, "레드와인", 60_000),
+    CHAMPAGNE(DRINK, "샴페인", 25_000);
+
+    private final MenuCategory menuCategory;
     private final String name;
     private final int price;
 
-    Menu(String category, String name, int price) {
-        this.category = category;
+    Menu(MenuCategory menuCategory, String name, int price) {
+        this.menuCategory = menuCategory;
         this.name = name;
         this.price = price;
     }
@@ -31,7 +37,7 @@ public enum Menu {
         return price;
     }
 
-    public final String getCategory() {
-        return category;
+    public final MenuCategory getCategory() {
+        return menuCategory;
     }
 }
