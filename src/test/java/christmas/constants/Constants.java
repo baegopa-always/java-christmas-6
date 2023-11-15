d0822526da1e0144eaf6470e266e22f3db209f25
dj <fjzl12@naver.com>
2023-11-15 23:37:31 +0900

    test(Constants): 테스트용 상수 구현

---
+++ src/test/java/christmas/constants/Constants.java
@@ -0,0 +1,13 @@
+package christmas.constants;
+
+public class Constants {
+    public static final int DAY_1 = 1;
+    public static final int DAY_3 = 3;
+    public static final int DAY_25 = 25;
+    public static final int DAY_26 = 26;
+    public static final int DAY_31 = 31;
+    public static final int PRICE_3000 = 3_000;
+    public static final int PRICE_5000 = 5_000;
+    public static final int PRICE_10000 = 10_000;
+    public static final int PRICE_20000 = 20_000;
+}
