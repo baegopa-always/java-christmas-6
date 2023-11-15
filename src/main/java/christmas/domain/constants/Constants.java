a8efd04f45a2d87f83d1d545ae25d295e560e427
dj <fjzl12@naver.com>
2023-11-15 14:48:25 +0900

    refactor(Constants): 불필요 상수 제거

--- src/main/java/christmas/domain/constants/Constants.java
+++ src/main/java/christmas/domain/constants/Constants.java
@@ -5,8 +5,6 @@ import java.util.List;
 public class Constants {
     public static final int MIN_EA = 1;
     public static final int MAX_EA = 20;
-    public static final int ONE = 1;
-    public static final int TWO = 2;
     public static final int MIN_DAY = 1;
     public static final int MAX_DAY = 31;
     public static final List<Integer> SPECIAL_DAY = List.of(3, 10, 17, 24, 25, 31);
@@ -15,7 +13,7 @@ public class Constants {
     public static final int MIN_PRICE_FOR_GIFT = 120_000;
     public static final int MIN_PRICE_FOR_EVENT = 10_000;
     public static final int CHRISTMAS_DAY = 25;
-    public static final int SPECIAL_DISCOUNT_PRICE = 1000;
+    public static final int SPECIAL_DISCOUNT_PRICE = 1_000;
     public static final String CHRISTMAS_DISCOUNT = "크리스마스 디데이 할인";
     public static final String WEEKDAY_DISCOUNT = "평일 할인";
     public static final String WEEKEND_DISCOUNT = "주말 할인";
