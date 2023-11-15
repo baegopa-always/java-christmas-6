6ad0f72d5f6a6faab32beab40e9fa775a50a558f
dj <fjzl12@naver.com>
2023-11-15 23:41:41 +0900

    test(ExpectedTotalPriceTest): 예상 결제 금액 계산 테스트 구현

---
+++ src/test/java/christmas/calculatorTest/ExpectedTotalPriceTest.java
@@ -0,0 +1,45 @@
+package christmas.calculatorTest;
+
+import christmas.domain.Calculator;
+import christmas.domain.constants.Menu;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static christmas.constants.Constants.DAY_1;
+import static christmas.constants.Constants.DAY_3;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ExpectedTotalPriceTest {
+    private static final Map<Menu, Integer> MENU_INVENTORY_1 = Map.of(
+            Menu.STAKE, 20);
+    private static final Map<Menu, Integer> MENU_INVENTORY_2 = Map.of(
+            Menu.ICE_CREAM, 10);
+    private static final Map<Menu, Integer> MENU_INVENTORY_3 = Map.of(
+            Menu.CHOCO_CAKE, 8);
+
+    @DisplayName("예상 결제 금액 테스트1")
+    @Test
+    public void setExpectedTotalPrice1() {
+        final Calculator calculator = new Calculator(MENU_INVENTORY_1, DAY_1);
+        calculator.createBenefitDetails();
+        assertThat(calculator.calculateExpectedTotalPrice()).isEqualTo(1_058_540);
+    }
+
+    @DisplayName("예상 결제 금액 테스트2")
+    @Test
+    public void setExpectedTotalPrice2() {
+        final Calculator calculator = new Calculator(MENU_INVENTORY_2, DAY_3);
+        calculator.createBenefitDetails();
+        assertThat(calculator.calculateExpectedTotalPrice()).isEqualTo(27_570);
+    }
+
+    @DisplayName("예상 결제 금액 테스트3")
+    @Test
+    public void setExpectedTotalPrice3() {
+        final Calculator calculator = new Calculator(MENU_INVENTORY_3, DAY_3);
+        calculator.createBenefitDetails();
+        assertThat(calculator.calculateExpectedTotalPrice()).isEqualTo(101_616);
+    }
+}
